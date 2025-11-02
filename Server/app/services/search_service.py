8b3de6f1eb4fc83db6e28a2e2b6b26882a16e27d
TAMARTAMA <ts0556726468@gmail.com>
2025-11-02 10:09:59 +0200

    delete the commets

--- Server/app/services/search_service.py
+++ Server/app/services/search_service.py
@@ -1,14 +1,3 @@
-import requests
-
-# def send_data_to_server_search(url: str, keywords: list):
-#     payload = {"query": keywords}
-#     headers = {"Content-Type": "application/json"}
-#     try:
-#         response = requests.post(url, json=payload, headers=headers, timeout=None)
-#         return response.json()
-#     except Exception as e:
-#         return {"error": f"Error sending request to search server: {e}"}
-
 from opensearchpy import OpenSearch
 
 client = OpenSearch(
@@ -16,8 +5,6 @@ client = OpenSearch(
     http_auth=("admin", "MyStrongPassword123!"),
     use_ssl=False
 )
-
-# keywords = ["machine learning", "AI", "neural networks"]
 def send_data_to_server_search(url: str, keywords: list):
     merged_results = {}
 
@@ -27,7 +14,7 @@ def send_data_to_server_search(url: str, keywords: list):
             "query": {
                 "multi_match": {
                     "query": keyword,
-                    "fields": ["title^2", "text"],  # title מקבל פי2  משקל
+                    "fields": ["title^2", "text"], 
                     "type": "best_fields"
                 }
             }
@@ -40,16 +27,10 @@ def send_data_to_server_search(url: str, keywords: list):
             score = hit["_score"]
             source = hit["_source"]
             title = source.get("title", "No Title")
-            text = source.get("text", "")  # עד 500 תווים כדי לא להעמיס
+            text = source.get("text", "")
 
             if doc_id not in merged_results or merged_results[doc_id]["score"] < score:
                 merged_results[doc_id] = {"title": title, "score": score, "text": text}
 
-    # מיון לפי score ולקיחת 5 המובילים
     top5 = sorted(merged_results.values(), key=lambda x: x["score"], reverse=True)[:5]
-
-    # for doc in top5:
-    #     print(f"\nTitle: {doc['title']} | Score: {doc['score']:.3f}")
-    #     print(f"Text: {doc['text']}")
-    #     print("-" * 80)
     return {"results": top5}
