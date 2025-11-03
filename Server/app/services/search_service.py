6b366c4ffc1ec0f3064b9251c23be682959ddd65
Y1243 <y8425901@gmail.com>
2025-11-03 15:59:32 +0200

    merge from main to me

--- Server/app/services/search_service.py
+++ Server/app/services/search_service.py
@@ -1,10 +1,37 @@
-import requests
+from opensearchpy import OpenSearch
 
+client = OpenSearch(
+    hosts=[{"host": "localhost", "port": 9200}],
+    http_auth=("admin", "MyStrongPassword123!"),
+    use_ssl=False
+)
 def send_data_to_server_search(url: str, keywords: list):
-    payload = {"query": keywords}
-    headers = {"Content-Type": "application/json"}
+    body = {
+        "size": 10,
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "multi_match": {
+                            "query": kw,
+                            "fields": ["title^2", "text"],
+                            "type": "best_fields"
+                        }
+                    } for kw in keywords
+                ],
+                "minimum_should_match": 1  # לפחות מונח אחד צריך להתאים
+            }
+        }
+    }
     try:
-        response = requests.post(url, json=payload, headers=headers, timeout=None)
-        return response.json()
+        res = client.search(index="wikipedia", body=body)
+
+        hits = res.get("hits", {}).get("hits", [])
+        results = [{"title": hit["_source"].get("title", "No Title"), 
+            "score": hit.get("_score", 0)} 
+        for hit in hits]
+        return {"results": results}
     except Exception as e:
-        return {"error": f"Error sending request to search server: {e}"}
+        print("❌ שגיאה בביצוע חיפוש:", str(e))
+        return []
+        
