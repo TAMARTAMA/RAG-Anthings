c6970da90160141585c53addfc465b7ade521f1e
Tamar Heksher <ts0556726468@gmail.com>
2025-11-21 11:39:26 +0200

    Update CORS middleware configuration

--- Server/app/main.py
+++ Server/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Main Server Chatbot")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],#["http://localhost:5175", "http://127.0.0.1:5175","http://localhost:5176", "http://127.0.0.1:5176"]  ,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
